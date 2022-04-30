035f5e5abc1b68060e570407851e4e91f47689bc
Michał Ś <65869511+Mike920-dev@users.noreply.github.com>
2022-04-30 16:11:56 +0200

    Update day8_1.py

--- day 8/day8_1.py
+++ day 8/day8_1.py
@@ -3,7 +3,7 @@ with open("day_8.in") as puzzle_input:
     signals = [line.strip().split("|")[1].removeprefix(" ") for line in puzzle_input]
 
 signals = [signal.split(" ") for signal in signals] 
-numOfDivergentLetters = {2: 1, 4: 4, 3: 7, 7: 8} # set default keys for each [1, 4, 7, 8], keys is the length of the output value(num of unique letters) 
+numOfDivergentLetters = {2: 1, 4: 4, 3: 7, 7: 8} # set default keys for each [1, 4, 7, 8], a key is the length of the output value(num of unique letters) 
 
 output_values = [output_val for signal in signals for output_val in signal] # add all of the values from each of the line
 output_values = list(filter(lambda x: len(set(x)) in numOfDivergentLetters.keys(), output_values)) # delete values that aren't 1, 4, 7 or 8
